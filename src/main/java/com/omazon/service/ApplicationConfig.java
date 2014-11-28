a122a1f21ccff5e8268cc72cd2cc4aa591d5bb94
Omar Erminy <omrsin@gmail.com>
2014-11-28 18:06:18 +0100

    Created the web services for the Product and the Customer entities.

---
+++ src/main/java/com/omazon/service/ApplicationConfig.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.omazon.service;
+
+import java.util.Set;
+import javax.ws.rs.core.Application;
+
+/**
+ *
+ * @author omar
+ */
+@javax.ws.rs.ApplicationPath("webresources")
+public class ApplicationConfig extends Application {
+
+    @Override
+    public Set<Class<?>> getClasses() {
+        Set<Class<?>> resources = new java.util.HashSet<>();
+        addRestResourceClasses(resources);
+        return resources;
+    }
+
+    /**
+     * Do not modify addRestResourceClasses() method.
+     * It is automatically populated with
+     * all resources defined in the project.
+     * If required, comment out calling this method in getClasses().
+     */
+    private void addRestResourceClasses(Set<Class<?>> resources) {
+        resources.add(com.omazon.service.CustomersFacadeREST.class);
+        resources.add(com.omazon.service.ProductsFacadeREST.class);
+    }
+    
+}

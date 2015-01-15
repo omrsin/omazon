cb1fe3de973abf784f48263de081495afc052cdb
Omar Erminy <omrsin@gmail.com>
2015-01-15 22:56:51 +0100

    Removes the test database env config that was conflicting

--- src/test/java/com/omazon/EntitiesTest.java
+++ src/test/java/com/omazon/EntitiesTest.java
@@ -29,9 +29,9 @@ public class EntitiesTest {
     
     @BeforeClass
     public static void setUpClass() {
-        if (em == null) {
-            em = (EntityManager) Persistence.createEntityManagerFactory("omazon_test").createEntityManager();
-        }
+//        if (em == null) {
+//            em = (EntityManager) Persistence.createEntityManagerFactory("omazon_test").createEntityManager();
+//        }
     }
     
     @AfterClass
@@ -51,22 +51,22 @@ public class EntitiesTest {
     //
      @Test
      public void testCustomerWithOneOrder() {
-         em.getTransaction().begin();
-         
-         Customers customer = new Customers();
-         customer.setName("Omar");
-         customer.setEmail("omar@omar.com");
-         Orders order = new Orders();
-         order.setShipmentId(1);
-         em.persist(customer);
-         em.flush();
-         
-         customer.getOrders().add(order);
-         order.setCustomer(customer);
-         
-         em.merge(customer);
-         em.flush();
-         
-         em.getTransaction().commit();
+//         em.getTransaction().begin();
+//         
+//         Customers customer = new Customers();
+//         customer.setName("Omar");
+//         customer.setEmail("omar@omar.com");
+//         Orders order = new Orders();
+//         order.setShipmentId(1);
+//         em.persist(customer);
+//         em.flush();
+//         
+//         customer.getOrders().add(order);
+//         order.setCustomer(customer);
+//         
+//         em.merge(customer);
+//         em.flush();
+//         
+//         em.getTransaction().commit();
      }
 }
